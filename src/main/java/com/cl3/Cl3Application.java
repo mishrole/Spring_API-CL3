b2ae586a6735fd521b7ba48c1d8b1059298e4d16
Mitchell Rodríguez <mitrodle@gmail.com>
2021-06-08 01:19:42 -0500

    Initial commit

---
+++ src/main/java/com/cl3/Cl3Application.java
@@ -0,0 +1,13 @@
+package com.cl3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl3Application.class, args);
+	}
+
+}
